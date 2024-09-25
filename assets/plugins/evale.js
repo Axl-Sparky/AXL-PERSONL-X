64cf173ac3efd427c41b6d08a0f792cc5364f2d4
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-09-25 21:43:44 +0530

    Update evale.js

--- assets/plugins/evale.js
+++ assets/plugins/evale.js
@@ -1,1 +1,25 @@
+const {
+    command,
+	getJson,
+  } = require("../../lib/");
+  const util = require("util");
 
+  command(
+    {
+      on: "text",
+      fromMe: true,
+      desc: "eval",
+      type: "user",
+    },
+    async (message, match) => {
+        if (match.startsWith("₹")) {
+			try {
+				let evaled = await eval(`(async () => { ${match.replace("₹", "")} })()`);
+				if (typeof evaled !== "string") evaled = util.inspect(evaled);
+				await message.reply(`${evaled}`)
+			} catch (err) {
+				await message.reply(`${util.format(err)}`);
+			}
+		}
+    }
+  );
