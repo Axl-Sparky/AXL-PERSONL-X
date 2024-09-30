918d4517e68c24d39d6f3234cd0357092ffdb425
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-09-30 11:09:47 +0530

    Update extrabutton.js

--- assets/plugins/extrabutton.js
+++ assets/plugins/extrabutton.js
@@ -7,7 +7,7 @@ command(
     type: "user",
   },
   async (message, match) => {
-    let whyxxx = ("*_Instagram Video/Photo Downloader ↓_* \n\n https://chat.whatsapp.com/CjY0gvu41yEJcicze7xhwk \n\n\n*_Public Bot Group ↓_* \n\n https://chat.whatsapp.com/DAD1FGg9dES0FpxtrUEheo");
+    let whyxxx = ("*_Public Bot Group ↓_* \n\n https://chat.whatsapp.com/DAD1FGg9dES0FpxtrUEheo");
 
 await message.reply(whyxxx);
   }
